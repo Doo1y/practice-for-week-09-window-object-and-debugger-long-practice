4a8808d979104b3444efae1c4d4b6a1bb94e10a8
Samuel Park <s.park111497@gmail.com>
2022-10-07 12:52:00 -0700

    finished assignment

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,21 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
 
-    // Your code here
+  // Your code here
+  document.title = "Sam's Portfolio";
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
 
-    // Your code here
+  // Your code here
+  document.body.children[0].children[0].innerText = "Sam";
 }
 
-function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
-     passage about yourself */
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
+   passage about yourself */
 
-    // Your code here
+  // Your code here
+  document.body.children[1].children[1].innerText = "Hello, my name is Sam and I like ham! Do you want some green eggs and ham?"
 }
